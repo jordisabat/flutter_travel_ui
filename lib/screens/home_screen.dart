2ac6d6e10dc8c7c25ba6acd2a0ab8fdfbec05003
Jordi Sabat <jsabat@gmail.com>
2020-09-28 21:52:22 +0200

    add bottombar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex, _selectedTab = 0;
 
   List<IconData> _icons = [
     FontAwesomeIcons.plane,
@@ -49,32 +49,64 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView(
-        padding: EdgeInsets.symmetric(vertical: 30),
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 120.0),
-            child: Text(
-              'What would you like to find?',
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text(
+                'What would you like to find?',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map((item) => _buildIcon(item.key))
+                  .toList(),
+            ),
+            SizedBox(height: 20.0),
+            DestinationCarousel()
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (int value) {
+          setState(() {
+            _selectedTab = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            title: Text(''),
+            icon: Icon(
+              Icons.search,
+              size: 30.0,
+            ),
           ),
-          SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: _icons
-                .asMap()
-                .entries
-                .map((item) => _buildIcon(item.key))
-                .toList(),
+          BottomNavigationBarItem(
+            title: Text(''),
+            icon: Icon(
+              Icons.local_pizza,
+              size: 30.0,
+            ),
           ),
-          SizedBox(height: 20.0),
-          DestinationCarousel()
+          BottomNavigationBarItem(
+            title: Text(''),
+            icon: CircleAvatar(
+              radius: 15.0,
+              backgroundImage: NetworkImage('http://i.imgur.com/zL4Krbz.jpg'),
+            ),
+          ),
         ],
-      )),
+      ),
     );
   }
 }
